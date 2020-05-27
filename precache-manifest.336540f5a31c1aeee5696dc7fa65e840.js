098c36fa2ea3bc941e662c08dcc4632b9d012708
Md. Musfiqur Rahman <mrifat46@gmail.com>
2020-05-28 00:01:57 +0600

    Updates

---
+++ precache-manifest.336540f5a31c1aeee5696dc7fa65e840.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08158f00fa113c77d60a3fbefc45cd3d",
+    "url": "/portfolio-app/index.html"
+  },
+  {
+    "revision": "3a1c369215a646544479",
+    "url": "/portfolio-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7ccae4e0ff5bb682ed17",
+    "url": "/portfolio-app/static/js/2.a039b72c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-app/static/js/2.a039b72c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a1c369215a646544479",
+    "url": "/portfolio-app/static/js/main.0db0f6e9.chunk.js"
+  },
+  {
+    "revision": "872065cd36d721e16ad8",
+    "url": "/portfolio-app/static/js/runtime-main.0296a6ac.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
